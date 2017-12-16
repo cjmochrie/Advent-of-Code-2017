0f5c85566b0860bf959026a3777c398ed0f978bf
Cameron Mochrie <cmochrie@wealthsimple.com>
2017-12-16 18:45:53 -0500

    day 10 pt 2

--- src/problems/day_10.rs
+++ src/problems/day_10.rs
@@ -1,4 +1,5 @@
 const LIST_LENGTH: u32 = 256;
+const ROUNDS: u32 = 64;
 
 #[derive(Debug)]
 struct State {
@@ -20,14 +21,19 @@ impl State {
 
   fn reverse_list(position: u32, length: u32, list: &Vec<u32>) -> Vec<u32> {
     let mut sub_list = vec![];
+
+    // extract the part of the list that will be reversed and reverse it
     for index in position..(position + length) {
       sub_list.push(list[index as usize % LIST_LENGTH as usize]);
     }
     sub_list.reverse();
     
+    // add the remaining elements in the correct order
     for index in (position + length)..(position + LIST_LENGTH) {
       sub_list.push(list[index as usize % LIST_LENGTH as usize]);
     }
+
+    // Re-orient the list in its original positions
     let mut final_list = vec![0; LIST_LENGTH as usize];
     for (index, element) in sub_list .into_iter().enumerate() {
       final_list[(position as usize + index) % LIST_LENGTH as usize] = element;
@@ -46,7 +52,38 @@ pub fn part_1(data: String) -> String {
 }
 
 pub fn part_2(data: String) -> String {
-  String::from("foo")
+  let mut data = to_ascii(data);
+  data.append(&mut vec![17, 31, 73, 47, 23]);
+  let result = hash_rounds(data);
+  let result = to_dense_hash(result);
+  to_hex_string(result)
+}
+
+fn to_hex_string(bytes: Vec<u8>) -> String {
+  bytes.iter().map(|b| format!("{:02x}", b)).collect()
+}
+
+fn hash_rounds(lengths: Vec<u32>) -> Vec<u32> {
+  let mut state = State::new();
+  for _ in 0..ROUNDS {
+    for length in lengths.iter() {
+      state.advance(*length);
+    }
+  }
+  state.list
+}
+
+fn to_dense_hash(data: Vec<u32>) -> Vec<u8> {
+  let mut result = vec![];
+  for chunk in data[..].chunks(16) {
+    let dense = chunk.iter().fold(0, |accum, c| accum as u8 ^ *c as u8);
+    result.push(dense);
+  }
+  result
+}
+
+fn to_ascii(data: String) -> Vec<u32> {
+  data.trim().chars().map(|c| c as u32).collect()
 }
 
 fn read_lengths(data: String) -> Vec<u32> {
